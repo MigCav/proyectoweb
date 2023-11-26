e3699d174db01143770dd298efda8c6618bd6854
MiguelCabrera <miguelecabrera@gmail.com>
2023-11-26 16:41:54 -0400

    prueba 3

--- web/webapp/urls.py
+++ web/webapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from webapp import views
 
 urlpatterns = [
-    path("home/", views.home, name='home'),
+    path("", views.home, name='home'),
     path("tienda/", views.tienda, name='tienda'),
     path("blog/", views.blog, name='blog'),
     path("servicios/", views.servicios, name= 'servicios'),
