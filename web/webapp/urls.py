6ccfcee4074243253729844c04e7a2b5d28c0c6a
MiguelCabrera <miguelecabrera@gmail.com>
2023-11-26 16:43:46 -0400

    probando cambio a mayuscula

--- web/webapp/urls.py
+++ web/webapp/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from webapp import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
-    path("tienda/", views.tienda, name='tienda'),
-    path("blog/", views.blog, name='blog'),
-    path("servicios/", views.servicios, name= 'servicios'),
-    path("contacto/", views.contacto, name='contacto'),
+    path("", views.home, name='Home'),
+    path("tienda/", views.tienda, name='Tienda'),
+    path("blog/", views.blog, name='Blog'),
+    path("servicios/", views.servicios, name= 'Servicios'),
+    path("contacto/", views.contacto, name='Contacto'),
     
     
 ]
