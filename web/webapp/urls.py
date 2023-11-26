79ce13a908d2a842fd7169a9e048656c5924a714
MiguelCabrera <miguelecabrera@gmail.com>
2023-11-26 16:32:28 -0400

    plantilla padre y modificacion leve

--- web/webapp/urls.py
+++ web/webapp/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from webapp import views
 
 urlpatterns = [
-    path("home/", views.home),
-    path("tienda/", views.tienda),
-    path("blog/", views.blog),
-    path("servicios/", views.servicios),
-    path("contacto/", views.contacto),
+    path("home/", views.home, name='home'),
+    path("tienda/", views.tienda, name='tienda'),
+    path("blog/", views.blog, name='blog'),
+    path("servicios/", views.servicios, name= 'servicios'),
+    path("contacto/", views.contacto, name='contacto'),
     
     
 ]
