a5caac29fd3e91da8a5c6e56e3bb105e85fc9206
MiguelCabrera <miguelecabrera@gmail.com>
2023-12-02 19:57:17 -0400

    creacion app blog, antes de las pruebas

--- web/blog/admin.py
+++ web/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import categoria, post
 
 # Register your models here.
+
+class postadmin(admin.ModelAdmin):
+    readonly_fields= ('created', 'updated')
+    
+class categoriaadmin(admin.ModelAdmin):
+    readonly_fields= ('created', 'updated')
+    
+admin.site.register(post, postadmin)
+admin.site.register(categoria, categoriaadmin)
