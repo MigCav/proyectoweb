ffb238aadf1cac89a3646c18daaaa8634510e5b0
MigCav <miguelecabrerav@gmail.com>
2023-12-10 13:42:11 -0400

    validando envio de datos correctamente

--- web/contacto/forms.py
+++ web/contacto/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class form_contacto(forms.Form):
     
     nombre = forms.CharField(label= 'Nombre', required=True)
-    email  = forms.CharField(label='Email', required=True)
+    email  = forms.EmailField(label='Email', required=True)
     contenido = forms.CharField(label='Contenido', widget=forms.Textarea)
     
--- web/contacto/views.py
+++ web/contacto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import form_contacto
 
@@ -8,6 +8,20 @@ from .forms import form_contacto
 def contacto(request):
     
     formulario_contacto = form_contacto()
+    
+    if request.method == "POST":
+        
+        """le decimos que rescate la info del POST y la cargue en el formulario"""
+        formulario_contacto = form_contacto(data=request.POST)
+        
+        if formulario_contacto.is_valid():
+            """si el formulario es valido que almacene cada cosa en la variable"""
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+            
+            """luego redirecciono a la pagina para avisar que se envio correctamente la informacion"""
+            return redirect("/contacto/?valido")            
     
     return render(request, "contacto/contacto.html", {'form_contacto':formulario_contacto} )
 
