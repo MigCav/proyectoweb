0e658217fd9828008db7e16881b851997baf1b75
MigCav <miguelecabrerav@gmail.com>
2023-12-11 16:52:08 -0400

    completando envio de correos

--- web/contacto/views.py
+++ web/contacto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 
 from .forms import form_contacto
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
 # Create your views here.
@@ -19,9 +21,15 @@ def contacto(request):
             nombre = request.POST.get('nombre')
             email = request.POST.get('email')
             contenido = request.POST.get('contenido')
-            
-            """luego redirecciono a la pagina para avisar que se envio correctamente la informacion"""
-            return redirect("/contacto/?valido")            
+        
+            email_message = EmailMessage("Mensaje enviado de django", "el usuario de nombre {} de correo {} envia lo siguiente: \n\n {}"
+                                         .format(nombre,email, contenido), '', ["miguel_kratos@hotmail.com"])
+            try:
+                email_message.send()
+                """luego redirecciono a la pagina para avisar que se envio correctamente la informacion"""
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")            
     
-    return render(request, "contacto/contacto.html", {'form_contacto':formulario_contacto} )
+    return render(request, "contacto/contacto.html", {'form_contacto':formulario_contacto} ) 
 
