5948ad4b8dc5cb937a5e459218a96568dc6ba77b
MigCav <miguelecabrerav@gmail.com>
2023-12-19 15:35:59 -0400

    creando views del carrito

--- web/carro/carro.py
+++ web/carro/carro.py
@@ -1,6 +1,61 @@
 class Carro:
-    def __init__(self) -> None:
+    def __init__(self):
         self.request = request
-        self.session = request.session
+        
+        #se mantiene el id de la sesion
+        self.session = request.session      
         carro = self.session.get('carro')
         
+        if not carro:
+            #si no hay carro crea uno nuevo con un diccionario vacio
+            carro = self.session['carro']= {}
+        else:
+            #si hay carro en la sesion lo recupera
+            self.carro = carro
+            
+            
+    def agregar(self, producto):
+        #se verifica si el id del producto no esta en las claves del diccionario
+        if (str(producto.id) not in self.carro.keys()):
+            #especifico lo que se mostrara en el carrito
+            self.carro[producto.id]= {
+                'producto_id':producto.id,
+                'nombre': producto.nombre,
+                'precio': str(producto.precio),
+                'cantidad':1,
+                'imagen': producto.imagen.url
+            }
+        else:
+            for key,value in self.carro.items():
+                if key == str(producto.id):
+                    value['cantidad'] = value['cantidad']+1
+                    break
+                
+        self.guardar_carro
+        
+    def guardar_carro(self):
+        self.session['carro'] = self.carro
+        self.session.modified = True
+        
+    def eliminar(self,producto):
+        producto.id = str(producto.id)
+        if producto.id in self.carro():
+            del self.carro[producto]
+            self.guardar_carro()
+            
+    def restar_productos(self, producto):
+        #recorremos los valores del diccionario para encontrar el producto
+        for key,value in self.carro.items():
+            if key == str(producto.id):
+                #le restamos 1 a la cantidad
+                value['cantidad'] = value['cantidad']-1
+                #si el valor es menor a 1 se elimina el producto
+                if value['cantidad'] <1:
+                    self.eliminar(producto)
+                break
+            
+        self.guardar_carro()
+            
+    def limpiar_carro(self):
+        self.session['carro'] = {}
+        self.session.modified = True
--- web/carro/views.py
+++ web/carro/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
+from .carro import Carro
+from tienda.models import producto
+from django.shortcuts import redirect
 # Create your views here.
+
+def agregar_producto(request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto_carro = producto.objects.get(producto_id)
+    
+    carro.agregar(producto=producto_carro)
+     
+    return redirect('tienda')
+
+def eliminar_producto(request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto_carro = producto.objects.get(producto_id)
+    
+    carro.eliminar(producto=producto_carro)
+     
+    return redirect('tienda')
+
+def restar_producto(request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto_carro = producto.objects.get(producto_id)
+    
+    carro.restar_productos(producto=producto_carro)
+     
+    return redirect('tienda')
+
+def limpiar_carro(request, producto_id):
+    
+    carro = Carro(request)
+    
+    carro.limpiar_carro()
+    return redirect('tienda')
